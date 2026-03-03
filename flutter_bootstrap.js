9d99988b8a2a38acee76c044b95cc39388fcc9e5
zmsp <zobair.career@gmail.com>
2026-03-03 10:54:35 +0000

    Deploy build from 1cf0ffb595f8e2ee484f43385e2886e2ddde819b

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2298312714" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "977975557" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
